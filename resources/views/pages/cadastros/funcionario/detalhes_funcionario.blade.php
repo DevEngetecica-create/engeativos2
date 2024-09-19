d0142b6d91d795f84a654cb1878339b57423173f
bruno_klemz <bruno.klemz@engetecnica.com.br>
2024-09-19 14:40:19 -0300

    alterações pagina detalhes funcionario

--- resources/views/pages/cadastros/funcionario/detalhes_funcionario.blade.php
+++ resources/views/pages/cadastros/funcionario/detalhes_funcionario.blade.php
@@ -6,9 +6,9 @@
 
 <style>
     @keyframes blink {
-    0% { opacity: 1; }
-    50% { opacity: 0; }
-    100% { opacity: 1; }
+        0% { opacity: 1; }
+        50% { opacity: 0; }
+        100% { opacity: 1; }
     }
     
     .loading-text {
@@ -44,207 +44,109 @@
         animation-delay: 1.2s;
         font-size:20px;
     }
+    
+    .nowrap {
+        white-space: nowrap; /* Impede quebra de linha */
+    }
+
+    /* Estilos para o header e footer com imagens fluidas */
+    header, footer {
+        width: 100%;
+        height: 150px; 
+        background-size: cover; 
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+    /* Ajustes para dispositivos móveis */
+    @media (max-width: 768px) {
+        header, footer {
+            height: 120px; 
+            background-size: cover; 
+            margin-bottom: -150px;
+        }
+    }
+
+    header {
+        background-image: url('{{ asset("build/images/usuarios/header.png") }}'); /* Caminho da imagem do header */
+    }
+
+    footer {
+        background-image: url('{{ asset("build/images/usuarios/footer.png") }}'); /* Caminho da imagem do footer */
+    }
+
+    /* Limita a largura máxima do container e centraliza */
+    .custom-container {
+        max-width: 500px; /* Define a largura máxima no desktop */
+        width: 100%;
+        margin: 0 auto; /* Centraliza o container no desktop */
+        padding: 0 15px; /* Adiciona um pouco de padding nas laterais */
+    }
+    /* Alinhar os itens à esquerda */
+    .table th, .table td {
+        text-align: left; /* Alinha os itens à esquerda */
+    }
 
 </style>
 
-<div class="container p-5">
-    <div class="col-12 btn bg-warning bg-gradient waves-effect waves-light pt-4 mb-4 mb-lg-3 pb-lg-4">
-        <div class="row g-4">
-            <div class="col-auto m-auto mt-sm-3 mt-xl-5 mb-0">
-                <img src="{{ URL::asset('build/images/icones/LogoMarca - Horizontal.svg') }}" alt="" height="50">
-            </div>
-            
-            <div class="col-auto text-center">
-                
-                <div class="avatar-lg">
-                    @if ($store->imagem)
-                        <img src="{{ asset('imagens/usuarios') }}/{{ $store->id }}" class="img-thumbnail rounded-circle" />
-                    @else
-                        <img src="{{ asset('imagens/usuarios/lista-de-usuarios.png') }}"
-                            class="img-thumbnail rounded-circle" />
-                    @endif
-                    
-                </div>
-            </div>
-            
-            
+<!-- Header -->
+<header></header>
 
-            {{-- dd("break") --}}
-            <!--end col-->
-            <div class="col">
-                <div class="p-2">
-                    <h3 class="text-white mb-1">{{ $store->nome }}</h3>
-                    <p class="text-white text-opacity-75">{{ $store->funcao->funcao ?? 'Sem reg.' }}</p>
-                    <div class="hstack text-white-50 gap-1">
-                       
-                    </div>
-                </div>
-            </div>
-        
-
-        </div>
-        <!--end row-->
-    </div>
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div>
-                <!-- Tab panes -->
-                <div class="tab-content pt-4 text-muted">
-                    <div class="tab-pane active" id="overview-tab" role="tabpanel">
-                        <div class="row">
-                            <div class="col-sm-12 col-xl-3">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title mb-3">Informações</h5>
-                                        <div class="table-responsive">
-                                            <table class="table table-borderless mb-0">
-                                                <tbody>
-                                                    <tr>
-                                                        <th class="ps-0" scope="row">Matrícula :</th>
-                                                        <td class="text-muted">{{ $store->matricula }}</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th class="ps-0" scope="row">Nome completo :</th>
-                                                        <td class="text-muted">{{ $store->nome }}</td>
-                                                    </tr>
-                                                  
-                                                    <tr>
-                                                        <th class="ps-0" scope="row">Contato :</th>
-                                                        <td class="text-muted">{{ $store->celular }}</td>
-                                                    </tr>
-                                                   
-                                                    <tr>
-                                                        <th class="ps-0" scope="row">Status</th>
-                                                        <td class="text-muted">{{ $store->status }}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div><!-- end card body -->
-                                </div><!-- end card -->
-                            </div>
-                            <!--end col-->
-                            
-                               <div class="col-sm-12 col-xl-9">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title m1-4">Qualificações</h5>
-                                        <hr class="m-0 p-0 mb-5 text-warning">
-
-                                        <div class="d-flex flex-wrap gap-2 fs-15">
-
-                                            <div class="row row-cols-1 row-cols-md-3 g-4">
-                                                @foreach ($qualificacao_funcoes as $qualificacao)
-                                                    <div class="col">
-                                                        <div class="card h-100">
-                                                            <h5 class="mx-3">
-                                                                {{ $qualificacao->qualificacoes->nome_qualificacao ?? 'Sem reg' }}
-                                                            </h5>
-                                                            <hr class="m-0 p-0">
-                                                            <div class="card-body pb-0 ">
-                                                                @php
-                                                                    $situacao = $qualificacao->situacao_doc ?? $qualificacao->situacao
-                                                                @endphp
-                                                                    
-                                                                 
-                                                                @if($situacao == 1 )
-                                                                
-                                                                        <p class="mb-2">
-                                                                            <small><strong>Situação: </strong> 
-                                                                                <button type="button" class="btn btn-warning btn-sm btn-label waves-effect waves-light">
-                                                                                    <i class="ri-check-double-line label-icon align-middle fs-16 me-2"></i>
-                                                                                     Pendente
-                                                                                </button>
-                                                                            </small>
-                                                                        </p>
-                                                                
-                                                                @elseif($situacao == 2)
-                                                                
-                                                                    <p class="mb-2">
-                                                                        <small><strong>Situação: </strong> 
-                                                                            <button type="button" class="btn btn-success btn-sm btn-label waves-effect waves-light">
-                                                                                <i class="ri-check-double-line label-icon align-middle fs-16 me-2"></i>
-                                                                                 Aprovado
-                                                                            </button>
-                                                                        </small>
-                                                                    </p>
-                                                                    
-                                                                @elseif($situacao == 18)
-                                                                
-                                                                    <p class="mb-2">
-                                                                        <small><strong>Situação: </strong> 
-                                                                            <button type="button" class="btn btn-danger btn-sm btn-label waves-effect waves-light">
-                                                                                <i class="ri-check-double-line label-icon align-middle fs-16 me-2"></i>
-                                                                                 Reprovado
-                                                                            </button>
-                                                                        </small>
-                                                                    </p>
-                                                                
-                                                                    
-                                                                @endif
-                                                                
-                                                              {{--dd($qualificacao)--}}
-                                                                
-                                                               @if(isset($qualificacao->qualificacoes) && $qualificacao->qualificacoes->publica == 1 && $situacao == 2)
-                                                                <p class="mb-0">
-                                                                    <small><strong>Download: </strong> 
-                                                                        <a href="{{ route('download.documento.publico', $qualificacao->id_anexos ?? 0) }}">
-                                                                            <button type="button"
-                                                                                class="btn btn-outline-success btn-sm waves-effect waves-light material-shadow-none">
-                                                                                <i class="mdi mdi-cloud-download"></i>
-                                                                            </button>
-                                                                        </a>
-                                                                    </small>
-                                                                </p>
-                                                                
-                                                                @else
-                                                                
-                                                                 <p class="mb-0">
-                                                                        <small class=text-danger><strong>Download: </strong> 
-                                                                            
-                                                                                 Não permitido
-                                                                         
-                                                                        </small>
-                                                                    </p>
-                                                                @endif
-                                                                
-                                                            </div>
-                                                            <div class="card-footer">
-                                                                <small class="text-muted">Data da validade:
-                                                                
-                                                                    @if($qualificacao->data_aprovacao)
-                                                                        {{ Tratamento::dateBr($qualificacao->data_validade_doc) }}
-                                                                    @else
-                                                                        Sem registro
-                                                                    @endif    
-                                                                </small>
-                                                            </div>
-                                                        </div>
-
-                                                    </div>
-                                                @endforeach
-                                            </div><!-- end card body -->
-                                        </div><!-- end card -->
-                                    </div>
-                                    <!--end col-->
-                                </div>
-                                <!--end row-->
-                            </div>
-
-                        </div>
-                        <!--end row-->
-                    </div>
-
-                 
-                </div>
-                <!--end tab-content-->
+<!-- Troca de container-fluid para custom container -->
+<div class="custom-container d-flex flex-column align-items-center justify-content-center content" style="min-height: 100vh;">
+    <!-- Imagem centralizada -->
+    <div class="text-center mb-4" style="width: 100%;">
+        <div class="d-flex justify-content-center">
+            <div class="avatar-lg">
+                @if ($store->imagem)
+                    <img src="{{ asset('imagens/usuarios') }}/{{ $store->id }}" class="img-thumbnail rounded-circle" />
+                @else
+                    <img src="{{ asset('imagens/usuarios/lista-de-usuarios.png') }}" class="img-thumbnail rounded-circle" />
+                @endif
             </div>
         </div>
-        <!--end col-->
     </div>
-    <!--end row-->
 
-</div>
+    <!-- Bloco de citação e informações abaixo da imagem -->
+    <div class="col-lg-12 text-center">
+        <!-- Blockquote com nome e função -->
+        <blockquote class="blockquote custom-blockquote blockquote-outline blockquote-primary rounded mb-4 material-shadow">
+            <p class="text-body mb-2"><strong>{{ $store->nome }}</strong></p>
+            <p class="text-body mb-2"><strong>{{ $store->funcao->funcao ?? 'Sem reg.' }}</strong></p>
+        </blockquote>
+
+        <!-- Box de informações original -->
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title mb-3">Informações</h5>
+                <div class="table-responsive">
+                    <table class="table table-borderless mb-0">
+                        <tbody>
+                            <tr>
+                                <th class="ps-0 nowrap" scope="row">Matrícula :</th>
+                                <td class="text-muted">{{ $store->matricula }}</td>
+                            </tr>
+                            <tr>
+                                <th class="ps-0 nowrap" scope="row">Nome completo :</th>
+                                <td class="text-muted">{{ $store->nome }}</td>
+                            </tr>
+                            <tr>
+                                <th class="ps-0 nowrap" scope="row">Contato :</th>
+                                <td class="text-muted">{{ $store->celular }}</td>
+                            </tr>
+                            <tr>
+                                <th class="ps-0 nowrap" scope="row">Status :</th>
+                                <td class="text-muted">{{ $store->status }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div><!-- end card -->
+    </div><!-- end col-lg-12 -->
+</div><!-- end custom container -->
+
+<!-- Footer -->
+<footer></footer>
 
 @endsection
